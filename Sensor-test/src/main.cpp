b86146472cbf479445f7644065dfee596d72d773
ReFil <31960031+ReFil@users.noreply.github.com>
2023-07-31 16:25:12 +0100

    Update main.cpp

--- Sensor-test/src/main.cpp
+++ Sensor-test/src/main.cpp
@@ -3,26 +3,14 @@
 #include <TimeLib.h>
 #include <Wire.h>
 //#define FIDAS
-//#define SENSPS30
 #define SEN55
-//#define HPMA115
-//#define NEXTPM
-//#define SDS011
+#define SDS011
 
 #ifdef FIDAS
 #include "FIDAS200.h"
 #endif
-#ifdef SENSPS30
-#include "sps30.h"
-#endif
 #ifdef SEN55
 #include "SensirionI2CSen5x.h"
-#endif
-#ifdef HPMA115
-#include "HPMA115_Compact.h"
-#endif
-#ifdef NEXTPM
-#include "nextpm.h"
 #endif
 #ifdef SDS011
 #include "SdsDustSensor.h"
@@ -31,20 +19,15 @@
 #ifdef FIDAS
 FIDAS200 fidas200;
 #endif
-#ifdef SENSPS30
-SPS30 sps30;
-#endif
 #ifdef SEN55
 SensirionI2CSen5x sen5x;
 #endif
-#ifdef HPMA115
-HPMA115_Compact hpm;
-#endif
-#ifdef NEXTPM
-NextPM teranextpm;
-#endif
 #ifdef SDS011
-SdsDustSensor sds011(Serial5);
+SdsDustSensor sds0111(Serial2);
+SdsDustSensor sds0112(Serial3);
+SdsDustSensor sds0113(Serial4);
+SdsDustSensor sds0114(Serial5);
+
 #endif
 
 String filename;
@@ -78,7 +61,7 @@ void setup() {
    if (dataFile) // it opened OK
     {
     Serial.println("Writing headers to csv");
-    dataFile.println("DateTime,Fidas 200 PM2.5,SPS30 PM2.5,SEN55 PM2.5,HPMA115 PM2.5,NextPM PM2.5,SDS011 PM2.5");
+    dataFile.println("DateTime,Fidas 200 PM2.5,SEN55 PM2.5,SEN55 RH,SEN55 T,SDS011 1 PM2.5,SDS011 2 PM2.5,SDS011 3 PM2.5,SDS011 4 PM2.5");
     dataFile.close();
     Serial.println("Headers written");
     }
@@ -91,21 +74,10 @@ void setup() {
     Serial1.begin(FIDAS200_BAUD);
     fidas200.begin(&Serial1);
   #endif
-  #ifdef SENSPS30
-    //initialise SPS30
-    Wire.begin();
-    if(sps30.begin(I2C_COMMS)){
-      Serial.println("begin sps30");
-    }
-    if(!sps30.probe())
-      Serial.println("Failed to communicate with SPS30");
-    sps30.reset();
-    sps30.start();
-  #endif
   #ifdef SEN55
     //Initialise SEN55 on i2c 2
-    Wire1.begin();
-    sen5x.begin(Wire1);
+    Wire.begin();
+    sen5x.begin(Wire);
     uint16_t error;
     char errorMessage[256];
     error = sen5x.deviceReset();
@@ -121,36 +93,17 @@ void setup() {
         Serial.println(errorMessage);
     }
   #endif
-  #ifdef HPMA115
-    //Initialise HPMA115 On serial2
-    Serial2.begin(HPMA115_BAUD);
-    hpm.begin(&Serial2);
-    while (!hpm.isNewDataAvailable()) {
-      Serial.println("HPM still waking up...");
-      delay(1000);
-    }
-  #endif
-  #ifdef NEXTPM
-    //Initialise NextPM on Serial3
-    teranextpm.begin(&Serial3);
-    NextPM_test data_test;
-    teranextpm.get_state();
-    teranextpm.powerOnTest(data_test);
-    Serial.println(data_test.connected);
-	Serial.println(data_test.sleep);
-	Serial.println(data_test.degraded);
-	Serial.println(data_test.default_state);
-	Serial.println(data_test.notready);
-	Serial.println(data_test.heat_error);
-	Serial.println(data_test.TH_error);
-	Serial.println(data_test.fan_error);
-	Serial.println(data_test.memory_error);
-	Serial.println(data_test.laser_error);
-  #endif
   #ifdef SDS011
     //Initialise SDS011
-    sds011.begin();
-    sds011.wakeup();
+    sds0111.begin();
+    sds0111.wakeup();
+    sds0112.begin();
+    sds0112.wakeup();
+    sds0113.begin();
+    sds0113.wakeup();
+    sds0114.begin();
+    sds0114.wakeup();
+
   #endif
 }
 
@@ -162,10 +115,6 @@ void loop() {
   #ifdef FIDAS
   //Fidas
     float fidasPM25;
-  #endif
-  #ifdef SENSPS30
-  //SPS30
-    struct sps_values sps30val;
   #endif
   #ifdef SEN55
   //SEN55
@@ -181,18 +130,12 @@ void loop() {
     float sen55vocIndex;
     float sen55noxIndex;
   #endif
-  #ifdef HPMA115
-    //HPMA115
-    uint16_t hpma115pm25;
-  #endif
-  #ifdef NEXTPM
-    //NextPM
-    NextPM_dataPM nextpmdata_pm;
-    //NextPM_dataTH nextpmdata_th;
-  #endif
   #ifdef SDS011
     //SDS011
-    PmResult sdspm = sds011.queryPm();
+    PmResult sds1pm = sds0111.queryPm();
+    PmResult sds2pm = sds0112.queryPm();
+    PmResult sds3pm = sds0113.queryPm();
+    PmResult sds4pm = sds0114.queryPm();
   #endif
   while(true) {
   #ifdef FIDAS
@@ -201,11 +144,7 @@ void loop() {
     fidasPM25 = fidas200.getValue(PM25);
     Serial.println("Fidas 200 PM2.5: " + String(fidasPM25));
   #endif
-  #ifdef SENSPS30
-  //Read SPS30
-    sps30.GetValues(&sps30val); 
-    Serial.println("SPS30 PM2.5: " + String(sps30val.MassPM2));
-  #endif
+
   #ifdef SEN55
     //Read SEN55
     sen55error = sen5x.readMeasuredValues(
@@ -222,23 +161,16 @@ void loop() {
     Serial.println("SEN55 Teemp: " + String(sen55ambientTemperature));
     Serial.println("SEN55 RH: " + String(sen55ambientHumidity));
   #endif
-  #ifdef HPMA115
-    //Read HPMA115
-    if (hpm.isNewDataAvailable()) {
-      hpma115pm25 = hpm.getPM25();
-    }
-    Serial.println("HPMA115 PM2.5: " + String(hpma115pm25));
-  #endif
-  #ifdef NEXTPM
-    //Read NextPM
-    teranextpm.fetchDataPM(nextpmdata_pm, 10);
-    //teranextpm.fetchDataTH(nextpmdata_th);
-    Serial.println("NEXTPM PM2.5: " + String(nextpmdata_pm.PM2_5));
-  #endif
   #ifdef SDS011
     //Read SDS011
-    sdspm = sds011.queryPm();
-    Serial.println("SDS011 PM2.5: " + String(sdspm.pm25));
+    sds1pm = sds0111.queryPm();
+    sds2pm = sds0112.queryPm();
+    sds3pm = sds0113.queryPm();
+    sds4pm = sds0114.queryPm();
+    Serial.println("SDS011 1 PM2.5: " + String(sds1pm.pm25));
+    Serial.println("SDS011 2 PM2.5: " + String(sds2pm.pm25));
+    Serial.println("SDS011 3 PM2.5: " + String(sds3pm.pm25));
+    Serial.println("SDS011 4 PM2.5: " + String(sds4pm.pm25));
   #endif
   //debug printing
   
@@ -254,29 +186,25 @@ void loop() {
     #else
     dataFile.print("0, ");
     #endif
-    #ifdef SENSPS30
-    dataFile.print(String(sps30val.MassPM2) + ", ");
-    #else
-    dataFile.print("0, ");
-    #endif
     #ifdef SEN55
     dataFile.print(String(sen55massConcentrationPm2p5) + ", ");
+    dataFile.print(String(sen55ambientHumidity) + ", ");
+    dataFile.print(String(sen55ambientTemperature) + ", ");
     #else
     dataFile.print("0, ");
-    #endif
-    #ifdef HPMA115
-    dataFile.print(String(hpma115pm25) + ", ");
-    #else
+    dataFile.print("0, ");
     dataFile.print("0, ");
     #endif
-    #ifdef NEXTPM
-    dataFile.print(String(nextpmdata_pm.PM2_5) + ", ");
-    #else
-    dataFile.print("0, ");
-    #endif
+
     #ifdef SDS011
-    dataFile.print(String(sdspm.pm25));
+    dataFile.print(String(sds1pm.pm25));
+    dataFile.print(String(sds2pm.pm25));
+    dataFile.print(String(sds3pm.pm25));
+    dataFile.print(String(sds4pm.pm25));
     #else
+    dataFile.print("0, ");
+    dataFile.print("0, ");
+    dataFile.print("0, ");
     dataFile.print("0");
     #endif
     dataFile.println();
